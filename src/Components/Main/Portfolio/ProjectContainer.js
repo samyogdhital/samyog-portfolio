bbb38a4898e07269a10bccb6f7f4c43634592385
Samyog Dhital <dhitalsamyog123@gmail.com>
2022-01-14 20:12:08 +0545

    done with added projects

--- src/Components/Main/Portfolio/ProjectContainer.js
+++ src/Components/Main/Portfolio/ProjectContainer.js
@@ -3,8 +3,10 @@ import { ThemeContext } from "../../../context/ThemeContext";
 
 import cryptoLg from "../../../Assets/img/Portfolio items/img/coin700.jpg";
 import cryptoSm from "../../../Assets/img/Portfolio items/img/coin400.jpg";
-import portfolioLg from "../../../Assets/img/Portfolio items/img/portfolio700.jpg";
-import portfolioSm from "../../../Assets/img/Portfolio items/img/portfolio400.jpg";
+import ecommerceLg from "../../../Assets/img/Portfolio items/img/ecommerce700.jpg";
+import ecommerceSm from "../../../Assets/img/Portfolio items/img/ecommerce400.jpg";
+import hotelLg from "../../../Assets/img/Portfolio items/img/hotel700.jpg";
+import hotelSm from "../../../Assets/img/Portfolio items/img/hotel400.jpg";
 import todolistLg from "../../../Assets/img/Portfolio items/img/todolist700.jpg";
 import todolistSm from "../../../Assets/img/Portfolio items/img/todolist400.jpg";
 import blogLg from "../../../Assets/img/Portfolio items/img/blog700.jpg";
@@ -20,13 +22,33 @@ const ProjectContainer = () => {
   const { isLight } = useContext(ThemeContext);
   const projects = [
     {
+      image: [ecommerceSm, ecommerceLg],
+      alt: "Ecommerce App",
+      heading: "Ecommerce App",
+      p1: "Ecommerce webapp with React.",
+      l1: "https://github.com/samyog-dhital/ecommerce-project",
+      a1: "Github page for a project.",
+      l2: "https://ecommerce.samyogdhital.me/",
+      a2: "CryptoChecker app live website.",
+    },
+    {
+      image: [hotelSm, hotelLg],
+      alt: "Hotel Website!",
+      heading: "Hotel Website",
+      p1: "Static Website of a Hotel.",
+      l1: "https://github.com/samyog-dhital/",
+      a1: "Github page for a project.",
+      l2: "https://hotel.samyogdhital.me/",
+      a2: "CryptoChecker app live website.",
+    },
+    {
       image: [cryptoSm, cryptoLg],
       alt: "CryptoChecker",
       heading: "CryptoChecker",
       p1: "CryptoChecker displaying different coin data.",
       l1: "https://github.com/samyog-dhital/cryptocurrency",
       a1: "Github page for a project.",
-      l2: "https://www.samratdhital.com.np/",
+      l2: "https://crypto.samyogdhital.me/",
       a2: "CryptoChecker app live website.",
     },
     {
@@ -36,7 +58,7 @@ const ProjectContainer = () => {
       p1: "An app that lets you chat with others through websocket.",
       l1: "https://github.com/samyog-dhital/react-chat-original",
       a1: "Github page for a Portfolio website project.",
-      l2: "https://www.basantaregmi.com.np/",
+      l2: "https://chat.samyogdhital.me/",
       a2: "Portfolio project live website.",
     },
     {
